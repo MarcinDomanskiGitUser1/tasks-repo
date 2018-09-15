4608fe08d6c7381253267b67a58cf3fd4b240614
Marcin Domański <marcinjozefzielen@gmail.com>
2018-09-15 08:52:59 +0200

    Module 19.4 (some small changes)

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -33,7 +33,8 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (mail.getToCc() != null) {
+
+        if (mail.getToCc() != null && mail.getToCc() != "") {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
